d4a5a572581f181ae2fa7d065a31de54750e004c
Junjun Li <junjunli666@gmail.com>
2019-01-18 19:33:18 +0800

    动态后端接口

--- mock/Application/Home/Controller/DynamicController.class.php
+++ mock/Application/Home/Controller/DynamicController.class.php
@@ -7,26 +7,19 @@ class DynamicController extends CommonController {
     public function getList() {
 
         $data = [
+           
             [
                 'userid' => "hello_lijj@qq.com",
                 'avatar' => "../res/static/images/info-img.png",
                 'username' =>  "软件小哥哥",
                 "contents" => "今天天气很好呀",
-                "create_time" => "2018-12-22 13:24"
-            ],
-            [
-                'userid' => "hello_lijj@qq.com",
-                'avatar' => "../res/static/images/info-img.png",
-                'username' =>  "软件小哥哥",
-                "contents" => "今天天气很好呀",
-                "create_time" => "2018-12-22 13:24"
-            ],
-            [
-                'userid' => "hello_lijj@qq.com",
-                'avatar' => "../res/static/images/info-img.png",
-                'username' =>  "软件小哥哥",
-                "contents" => "今天天气很好呀",
-                "create_time" => "2018-12-22 13:24"
+                "create_time" => "2018-12-22 13:24",
+                "contents_img" => [
+                    "http://www.pptok.com/wp-content/uploads/2012/08/xunguang-4.jpg",
+                    "http://5b0988e595225.cdn.sohucs.com/images/20181219/aec27193b3834a1694f535ba307995ad.jpeg"
+                ],
+                "comments" => 2,
+                "stars" => 8
             ],
         ];
 
